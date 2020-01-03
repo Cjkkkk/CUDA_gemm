ad65a0d0641bdca5264c3444fbeec04dc0983bad
cjkkkk <shanbinke@gmail.com>
2020-01-04 00:39:14 +0800

    update template

--- src/cuda/dense.cu
+++ src/cuda/dense.cu
@@ -419,7 +419,6 @@ template <int BLOCK_SIZE> __global__ void MatrixMulCUDA4(
     reinterpret_cast<float4*> (C + N * ( BLOCK_SIZE * by + ty * 4 + 3) + BLOCK_SIZE * bx + tx * 4 )[0] = Csub[3];
 }
 
-#define offset(row, col, lda) ( (row) * (ld) + (col) )
 // global memory col
 template <
     int BLOCK_SIZE_M, 
@@ -442,80 +441,68 @@ __global__ void MatrixMulCUDA6(
     int tx = threadIdx.x;
     int ty = threadIdx.y;
     
-    // Block size
-    // const int bszx = blockDim.x;
-    const int bszy = blockDim.y;
-    
     // shared memory
     __shared__ float As[BLOCK_SIZE_M * BLOCK_SIZE_K];
     __shared__ float Bs[BLOCK_SIZE_K * BLOCK_SIZE_N];
-    // 16 registers
-    float4 Csub[4] = {
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}};
-    float4 b_vec;
+    
+    // registers for C
+    float accum[THREAD_SIZE_M][THREAD_SIZE_N];
+    for ( int i = 0 ; i < THREAD_SIZE_M ; i ++) {
+        for ( int j = 0 ; j < THREAD_SIZE_N ; j ++) {
+            accum[i][j] = 0;
+        }
+    }
 
-    int As_offset = 0;
-    int Bs_offset = 0;
+    // registers for A and B
+    float frag_a[THREAD_SIZE_M];
+    float frag_b[THREAD_SIZE_N];
     
     for (int tile_idx = 0 ; tile_idx < K / BLOCK_SIZE_K ;  tile_idx +=1) {
-        float* As_read = As + As_offset;
-        float* Bs_read = Bs + Bs_offset;
-
-        int idx = tx;
-        // load A tile
+        
         #pragma unroll
-        for ( int i = 0 ; i < BLOCK_SIZE_M / bszy ; i ++ ) {
-            reinterpret_cast<float4*>(
-                As_read + BLOCK_SIZE_K * (ty + bszy * i) + idx * 4)[0] 
-          = reinterpret_cast<float4*>(
-                A + K * (BLOCK_SIZE_M * by + ty + bszy * i ) + BLOCK_SIZE_K * tile_idx + idx * 4 )[0];
+        for ( int i = 0 ; i < 4 ; i ++ ) {
+            reinterpret_cast<float4*>(As + BLOCK_SIZE_K * (ty * 4 + i) + tx * 4)[0] 
+                = reinterpret_cast<float4*>(A + (BLOCK_SIZE_K * by + ty * 4 + i ) * K + BLOCK_SIZE_K * tile_idx + tx * 4 )[0];
+            reinterpret_cast<float4*>(Bs + BLOCK_SIZE_K * (ty * 4 + i) + tx * 4)[0] 
+                = reinterpret_cast<float4*>(B + (BLOCK_SIZE_K * tile_idx + ty * 4 + i ) * N + BLOCK_SIZE_K * bx + tx * 4 )[0];
         }
-
-        // load B tile
-        #pragma unroll
-        for ( int i = 0 ; i < BLOCK_SIZE_K / bszy ; i ++ ) {
-            reinterpret_cast<float4*>(
-                Bs_read + BLOCK_SIZE_N * (ty + bszy * i) + tx * 4)[0] 
-          = reinterpret_cast<float4*>(
-                B + N * (BLOCK_SIZE_K * tile_idx + ty + bszy * i ) + BLOCK_SIZE_N * bx + tx * 4 )[0];
-        }
+    
     
         __syncthreads();
 
         #pragma unroll
         for (int k = 0; k < BLOCK_SIZE_K; ++ k) {
-            b_vec = reinterpret_cast<float4*>(Bs_read + BLOCK_SIZE_N * k + tx * 4)[0];
+            #pragma unroll
+            for (int thread_y = 0; thread_y < THREAD_SIZE_M; ++thread_y) {
+                frag_a[thread_y] = As[BLOCK_SIZE_K *  (ty * THREAD_SIZE_M + thread_y) + k];
+            }
+
+            #pragma unroll
+            for (int thread_x = 0; thread_x < THREAD_SIZE_N; ++thread_x) {
+                frag_b[thread_x] = Bs[BLOCK_SIZE_N *  k + THREAD_SIZE_N * tx + thread_x];
+            }
             
-            Csub[0].x = fma(As_read[BLOCK_SIZE_K *  ty * 4      + k], b_vec.x, Csub[0].x);
-            Csub[0].y = fma(As_read[BLOCK_SIZE_K *  ty * 4      + k], b_vec.y, Csub[0].y);
-            Csub[0].z = fma(As_read[BLOCK_SIZE_K *  ty * 4      + k], b_vec.z, Csub[0].z);
-            Csub[0].w = fma(As_read[BLOCK_SIZE_K *  ty * 4      + k], b_vec.w, Csub[0].w);
-            Csub[1].x = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 1) + k], b_vec.x, Csub[1].x);
-            Csub[1].y = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 1) + k], b_vec.y, Csub[1].y);
-            Csub[1].z = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 1) + k], b_vec.z, Csub[1].z);
-            Csub[1].w = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 1) + k], b_vec.w, Csub[1].w);
-            Csub[2].x = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 2) + k], b_vec.x, Csub[2].x);
-            Csub[2].y = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 2) + k], b_vec.y, Csub[2].y);
-            Csub[2].z = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 2) + k], b_vec.z, Csub[2].z);
-            Csub[2].w = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 2) + k], b_vec.w, Csub[2].w);
-            Csub[3].x = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 3) + k], b_vec.x, Csub[3].x);
-            Csub[3].y = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 3) + k], b_vec.y, Csub[3].y);
-            Csub[3].z = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 3) + k], b_vec.z, Csub[3].z);
-            Csub[3].w = fma(As_read[BLOCK_SIZE_K * (ty * 4 + 3) + k], b_vec.w, Csub[3].w);
+            #pragma unroll
+            for (int thread_y = 0; thread_y < THREAD_SIZE_M; ++thread_y) {
+                #pragma unroll
+                for (int thread_x = 0; thread_x < THREAD_SIZE_N; ++thread_x) {
+                    accum[thread_y][thread_x ] += frag_a[thread_y] * frag_b[thread_x];
+                }
+            }
             
         }
-        // As_offset ^= (BLOCK_SIZE_M * BLOCK_SIZE_K);
-        // Bs_offset ^= (BLOCK_SIZE_K * BLOCK_SIZE_N);
         __syncthreads();
     }
 
-    reinterpret_cast<float4*> (C + N * ( BLOCK_SIZE_M * by + ty * 4 ) + BLOCK_SIZE_N * bx + tx * 4 )[0] = Csub[0];
-    reinterpret_cast<float4*> (C + N * ( BLOCK_SIZE_M * by + ty * 4 + 1) + BLOCK_SIZE_N * bx + tx * 4 )[0] = Csub[1];
-    reinterpret_cast<float4*> (C + N * ( BLOCK_SIZE_M * by + ty * 4 + 2) + BLOCK_SIZE_N * bx + tx * 4 )[0] = Csub[2];
-    reinterpret_cast<float4*> (C + N * ( BLOCK_SIZE_M * by + ty * 4 + 3) + BLOCK_SIZE_N * bx + tx * 4 )[0] = Csub[3];
+    // store back to C
+    #pragma unroll
+    for (int thread_y = 0; thread_y < THREAD_SIZE_M; ++thread_y) {
+        #pragma unroll
+        for (int thread_x = 0; thread_x < THREAD_SIZE_N; ++thread_x) {
+            C[N * ( BLOCK_SIZE_M * by + ty * THREAD_SIZE_M + thread_y) + BLOCK_SIZE_N * bx + tx * THREAD_SIZE_N + thread_x]
+            = accum[thread_y][thread_x];
+        }
+    }
 }
 
 // TODO add shuffle to enable GPU write back col
